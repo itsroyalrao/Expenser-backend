b66d22f562f9aa512f5fde711524e00b46675ed2
Mohit Yadav <my701319@gmail.com>
2023-11-20 20:40:48 +0530

    eighteenth commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { callbackGoogle, logoutGoogle } from "../components/auth.js";
 import passport from "passport";
 
 const router = express.Router();
@@ -12,38 +11,18 @@ router.get(
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
-  callbackGoogle
+  function (req, res) {
+    res.redirect(
+      `https://expenser-v1.netlify.app?user=${encodeURIComponent(
+        JSON.stringify(req.user._json)
+      )}`
+    );
+  }
 );
 
-router.get("/logout", logoutGoogle);
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("https://expenser-v1.netlify.app");
+});
 
 export default router;
-
-// import express from "express";
-// import passport from "passport";
-
-// const router = express.Router();
-
-// router.get(
-//   "/google",
-//   passport.authenticate("google", { scope: ["email", "profile"] })
-// );
-
-// router.get(
-//   "/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/login" }),
-//   function (req, res) {
-//     res.redirect(
-//       `https://expenser-v1.netlify.app?user=${encodeURIComponent(
-//         JSON.stringify(req.user._json)
-//       )}`
-//     );
-//   }
-// );
-
-// router.get("/logout", (req, res) => {
-//   req.logOut();
-//   res.redirect("https://expenser-v1.netlify.app");
-// });
-
-// export default router;
