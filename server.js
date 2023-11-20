4a363ad3c252d55e9ef5c5988f804bb45e3189b8
Mohit Yadav <my701319@gmail.com>
2023-11-20 10:42:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config();
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is listening on port ${port}!`))
